e9f7dd3698f3363f3609dcbecd71a4f7420e5386
Jamie Gaehring <gaehj457@gmail.com>
2023-07-22 16:53:46 -0400

    WIP: SavedState impl

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -24,6 +24,68 @@ enum SaveError {
     Format,
 }
 
+impl SavedState {
+    fn path() -> std::path::PathBuf {
+        let mut path = std::env::current_dir().unwrap_or_default();
+        path.push("rgo_early_warning.json");
+
+        path
+    }
+
+    async fn load() -> Result<SavedState, LoadError> {
+        use async_std::{self, prelude::*};
+
+        let mut contents = String::new();
+
+        let mut file = async_std::fs::File::open(Self::path())
+            .await
+            .map_err(|_| LoadError::File)?;
+
+        file.read_to_string(&mut contents)
+            .await
+            .map_err(|_| LoadError::File)?;
+
+        serde_json::from_str(&contents).map_err(|_| LoadError::Format)
+    }
+
+    pub fn print(&self) {
+        let path = Self::path();
+        println!("Path: {:#?}", path.display());
+    }
+
+    async fn save(self) -> Result<(), SaveError> {
+        use async_std::prelude::*;
+    
+        let json = serde_json::to_string_pretty(&self)
+            .map_err(|_| SaveError::Format)?;
+
+        let path = Self::path();
+        println!("Path:");
+        println!("{:#?}", path);
+
+        if let Some(dir) = path.parent() {
+            async_std::fs::create_dir_all(dir)
+                .await
+                .map_err(|_| SaveError::File)?;
+        }
+
+        
+        let mut file = async_std::fs::File::create(path)
+            .await
+            .map_err(|_| SaveError::File)?;
+
+        file.write_all(json.as_bytes())
+            .await
+            .map_err(|_| SaveError::Write)?;
+        
+
+        // This is a simple way to save at most once every couple seconds
+        async_std::task::sleep(std::time::Duration::from_secs(2)).await;
+
+        Ok(())
+    }
+}
+
 #[derive(Clone, Serialize, Deserialize, Default)]
 struct SavedState {
     farms: Vec<Farm>
